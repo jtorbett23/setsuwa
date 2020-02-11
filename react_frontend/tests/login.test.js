25d4eb102ab8a29f7a081b32e900258b602336f8
Josh Torbett <josh.torbett@getfutureproof.co.uk>
2020-02-11 20:00:07 +0000

    update tests

--- react_frontend/tests/login.test.js
+++ react_frontend/tests/login.test.js
@@ -9,30 +9,4 @@ describe('Render tests', () => {
         ReactDOM.render(<Login />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
-    it("Shows 'logged in' when logged in", () => {
-        let simFn = jest.fn();
-        // let simulatedData = {};
-        const wrapper = shallow(<Login 
-            handleUsername={simFn}
-            handlePassword={simFn}
-            handleLogin={simFn}
-            handleRegister={simFn} 
-            loggedInMessage={"Logged in"}
-            registerMessage={"User has been created. Now please log in"} />);
-        let message = wrapper.find('#loginMsg');
-        expect(message.text()).toEqual("Logged in");
-    });
-    it("Shows 'logged in' when logged in", () => {
-        let simFn = jest.fn();
-        // let simulatedData = {handleUsername={simFn}, handlePassword={simFn}, handleLogin={simFn}, handleRegister={simFn}, loggedInMessage={loggedInMessage: "Logged in"}, registerMessage={registerMessage: "User has been created. Now please log in"}};
-        const wrapper = shallow(<Login 
-            handleUsername={simFn}
-            handlePassword={simFn}
-            handleLogin={simFn}
-            handleRegister={simFn} 
-            loggedInMessage={"Logged in"}
-            registerMessage={"User has been created. Now please log in"} />);
-        let message = wrapper.find('#RegisterMsg');
-        expect(message.text()).toEqual("User has been created. Now please log in");
-    })
 })
