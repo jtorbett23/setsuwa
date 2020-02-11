100001127683132ca4f92af3dd7d4ff7e5de111b
Aaron M <aaron_213@hotmail.co.uk>
2020-02-11 19:47:16 +0000

    Creat register page for non-users

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -3,22 +3,31 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
 import axios from 'axios';
-import Posts from './containers/Posts'
 import Login from './components/Login'
+import Register from './components/Register'
+import MakePost from './components/MakePost'
+import SinglePostHookContainer from './containers/SinglePostHookContainer'
+import Posts from './containers/Posts'
 import './static/css/App.css';
 
 export default class App extends Component {
   state = {
     loggedIn: false,
-    food: 'rice',
     username: null,
     password: null,
     user_id: null,
     loggedInMessage: false,
-    registerMessage: false
+    logInMessage: false,
+    registerPayload: null,
+    title: null,
+    blog: null,
+    tags: null,
+    post: null,
+    posts: null,
   }
 
   handleUsername (e) {
@@ -27,54 +36,69 @@ export default class App extends Component {
   handlePassword (e) {
     this.setState({password: e.target.value})
   }
+  handleTitle (e) {
+    this.setState({title: e.target.value})
+  }
+  handleBlog (e) {
+    this.setState({blog: e.target.value})
+  }
+  handleTags (e) {
+    this.setState({tags: e.target.value})
+  }
 
   getID() {
-    let headers = {
-      "Access-Control-Allow-Origin": "http://localhost:5000/",
-      "Access-Control-Allow-Methods": "GET, POST",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Headers": "Access-Control-Allow-Origin, Access-Control-Allow-Origin"
-    }
-
     axios.post(`http://localhost:5000/auth/login?username=${this.state.username}&password=${this.state.password}`)
       .then(res => {
         if(res.data.user_id) {
           this.setState({user_id: res.data.user_id})
           this.setState({loggedInMessage: "Logged in"})
+          this.setState({loggedIn: true})
         }
         if(res.data.message) {
-          this.setState({loggedInMessage: res.data.message})
+          this.setState({logInMessage: res.data.message})
+          this.setState({loggedIn: false})
         }
-        console.log(res.data)
       })
-  }
-  
-  createUser() {
-    let headers = {
-      "Access-Control-Allow-Origin": "http://localhost:5000/",
-      "Access-Control-Allow-Methods": "GET, POST",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Headers": "Access-Control-Allow-Origin, Access-Control-Allow-Origin"
+    }
+    
+    createUser() {
+      axios.post(`http://localhost:5000/auth/register?username=${this.state.username}&password=${this.state.password}`)
+      .then(res => {
+        this.setState({registerPayload: res})
+      })
+      .catch(err => {
+        this.setState({registerPayload: err.response})
+      })
+    }
+    
+    createPost() {
+      axios.post(`http://localhost:5000/db/post?user_id=${this.state.user_id}&title=${this.state.title}&content=${this.state.blog}&tag=${this.state.tags}`)
+      .then(res => {
+        if(res.status === 200) {
+            this.setState({post: res.data.post_id})
+            console.log(res.data.post_id)
+        }
+        if(res.status === 500) {
+          console.log('didnt work') // need to alter this
+        }
+      })
     }
 
-    axios.post(`http://localhost:5000/auth/register?username=${this.state.username}&password=${this.state.password}`)
-      .then(res => {
-        this.setState({registerMessage: res.data.message})
-      })
-  }
-
-  handleLogin(e) {
-    e.preventDefault();
-    this.getID()
-  }
-  handleRegister(e) {
-    e.preventDefault();
-    this.createUser()
-  }
+    handleLogin(e) {
+      e.preventDefault();
+      this.getID()
+    }
+    handleRegister(e) {
+      e.preventDefault();
+      this.createUser()
+    }
+    
+    logout() {
+      window.location.reload(false);
+    }
 
   componentDidUpdate() {
     console.log("woop")
-    console.log(this.state.user_id)
   }
 
   render() {
@@ -104,7 +128,7 @@ export default class App extends Component {
                           <Link to="/useraccount">User account</Link>
                       </li>
                       <li>
-                          <Link id="logout" to="/">Logout</Link>
+                          <Link onClick={this.logout} id="logout" to="/">Logout</Link>
                       </li>
                     </div>
                   }
@@ -114,25 +138,44 @@ export default class App extends Component {
                     </li>
                   }
               </ul>
+              <p id="loginMsg">{this.state.loggedInMessage !== false ? this.state.loggedInMessage : 'Logged out'}</p>
           </nav>
 
           <h1>Setsuwa</h1>
 
           <Switch>
+              <Route path="/post/:id">
+                   <SinglePostHookContainer />
+              </Route>
               <Route path="/login">
-                  <Login 
+                  {this.state.loggedIn ? <Redirect to="/" /> :
+                    <Login 
                   handleUsername={this.handleUsername.bind(this)} 
                   handlePassword={this.handlePassword.bind(this)} 
                   handleLogin={this.handleLogin.bind(this)} 
+                  logInMessage={this.state.logInMessage} />} 
+                  </Route>
+                  <Route path="/register">
+                  <Register
+                  handleUsername={this.handleUsername.bind(this)} 
+                  handlePassword={this.handlePassword.bind(this)}
                   handleRegister={this.handleRegister.bind(this)} 
-                  loggedInMessage={this.state.loggedInMessage} 
-                  registerMessage={this.state.registerMessage}/>
+                  registerPayload={this.state.registerPayload}/>
               </Route>
               <Route path="/users">
                   {/* <Users /> */}
               </Route>
+              <Route path="/createpost">
+                  {this.state.post !== null ? <Redirect to={`/post/${this.state.post}`} /> :
+                  <MakePost 
+                  handleTitle={this.handleTitle.bind(this)}
+                  handleBlog={this.handleBlog.bind(this)}
+                  handleTags={this.handleTags.bind(this)}
+                  createPost={this.createPost.bind(this)}/>}
+              </Route>
               <Route path="/">
-                  <Posts food={this.state.food} />
+                  {/* <Posts post={this.state.post} /> */}
+                  <p>This is the home page that will display "Posts"</p>
               </Route>
           </Switch>
         </div>
