de282c48aa4e497f233eb0b7631091ac719359f0
Aaron M <aaron_213@hotmail.co.uk>
2020-02-11 19:46:17 +0000

    Adjust how a post data is passed down, and code to display the post

--- react_frontend/src/containers/posts.js
+++ react_frontend/src/containers/posts.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import Post from '../components/Post'
+import Comments from './Comments'
 
 export default class Posts extends Component {
+    state = {
+        posts: this.props.posts,
+        comments: null
+    }
     render() {
         return (
             <div>
                 <p>Posts page</p>
-                <p>{this.props.food}</p>
-
+                {/* map through all the posts pass through */}
+                <Post post={this.props.post} />
+                <Comments />
             </div>
         )
     }
