fdf72aac92fc8909ba6a48e53157c912e924942e
Aaron M <aaron_213@hotmail.co.uk>
2020-02-12 11:29:25 +0000

    Ability to delete a post, only if you are the user who has created the post

--- react_frontend/src/containers/SinglePost.js
+++ react_frontend/src/containers/SinglePost.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 export default class SinglePost extends Component {
     state = {
-        post: null
+        post: null,
+        user_id: this.props.user_id,
+        deleted: false,
     }
 
     getPost() {
@@ -13,6 +16,13 @@ export default class SinglePost extends Component {
           })
       }
 
+    deletePost() {
+        axios.delete(`http://localhost:5000/db/post?post_id=${this.props.id}`)
+        .then(res => {
+            this.setState({deleted: true})
+        })
+    }
+        
     componentDidMount() {
         this.getPost()
     }
@@ -21,12 +31,15 @@ export default class SinglePost extends Component {
         return (
             <div>
                 {this.state.post !== null ? 
+                !this.state.deleted ?
                 <div>
                 <h3>{this.state.post.title}</h3>
                 <p>{this.state.post.content}</p>
                 <p>{this.state.post.tag}</p>
-                </div> 
-                : null}
+                {this.state.user_id === this.state.post.user_id ? <button onClick={this.deletePost.bind(this)}>Delete post</button> : null}
+                </div> : <p>Post deleted</p>
+                : <p>This post does not exist :L</p>}
+                {this.state.deleted === true ? <Redirect push to="/"/> : null}
             </div>
         )
     }
--- react_frontend/src/containers/SinglePostHookContainer.js
+++ react_frontend/src/containers/SinglePostHookContainer.js
@@ -2,12 +2,12 @@ import React from 'react'
 import {useParams} from 'react-router'
 import SinglePost from './SinglePost'
 
-export default function SinglePostHookContainer() {
+export default function SinglePostHookContainer(props) {
     const {id} = useParams();
     let query = id;
     return (
         <div className="Search">
-                <div> <SinglePost id={query} /> </div>
+                <div> <SinglePost id={query} user_id={props.user_id}/> </div>
         </div>
     )
 }
