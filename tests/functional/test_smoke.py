6bb87b8edf80e3674ce5b220f6a7188536b7cc85
Josh Torbett <josh.torbett@getfutureproof.co.uk>
2020-02-13 11:38:27 +0000

    remove test and update seed post data

--- tests/functional/test_smoke.py
+++
@@ -1,7 +0,0 @@
-#test '/' route to check pytest is working properly
-
-def test_app_running_route(test_client):
-    response = test_client.get('/running')
-    assert response.status_code == 200
-    assert b"app is running" in response.data
-
